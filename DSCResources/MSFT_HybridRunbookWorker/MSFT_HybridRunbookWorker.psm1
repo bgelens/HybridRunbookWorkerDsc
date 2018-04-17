bdf54b94e05b3c3f854316c8a42da11f66eee4b2
eamonoreilly <eamono@microsoft.com>
2018-04-17 13:56:48 -0700

    Check for user hybrid workers as system ones might be registered also.

--- DSCResources/MSFT_HybridRunbookWorker/MSFT_HybridRunbookWorker.psm1
+++ DSCResources/MSFT_HybridRunbookWorker/MSFT_HybridRunbookWorker.psm1
@@ -311,7 +311,21 @@ function TestRegModule
 #>
 function TestRegRegistry
 {
-    Test-Path -Path HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker
+    $Reg = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker
+    if ($null -eq $Reg) {
+        #newer version
+        Get-Item -Path HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker\*\* | 
+            ForEach-Object -Process {
+                if (Get-ChildItem -Path $_.PSPath |
+                    Where-Object -FilterScript {
+                        $_.PSChildName -eq 'User'
+                    }
+                ) {
+                    $_.PSChildName
+                }
+            }
+        }
+    $Reg
 }
 
 <#
